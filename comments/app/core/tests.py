cc792133fb22de9060179dd81dae97ed6096ee0e
odulaja philip <odulajaphilip@gmail.com>
2022-09-04 13:46:30 +0100

    Maps and filter

--- comments/app/core/tests.py
+++ comments/app/core/tests.py
@@ -1,11 +1,11 @@
 # from django.test import TestCase
 
 # Create your tests here.
-letters = ('x', 'Y', 'z' and 'G' and 'p')
-x = False
-while not x:
-    for _ in letters:
-        print(_)
 
-        print(letters)
-    x = True
+def multipy(*args):
+    sum = 1
+    for num in args:
+        sum *= num
+    return sum
+
+print(multipy(6, 7, 8, 8))
