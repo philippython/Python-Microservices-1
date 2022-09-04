169156e23658a180b5471e1096a3ff59214d525a
odulaja philip <odulajaphilip@gmail.com>
2022-09-04 14:48:08 +0100

    exercise completed

--- comments/app/core/tests.py
+++ comments/app/core/tests.py
@@ -23,9 +23,32 @@ result = list(map(round, circle_areas, range(1, 7)))
 print(result)
 
 # filtering 
-past = list(filter(lambda word , char:  char in word, ["laughed", "pray", "killed", "raped", "race"]))
+past = list(filter(lambda word :  'd' in word, ["laughed", "pray", "killed", "raped", "race"]))
 print(past)
 
 # reducing
 multi = reduce(multipy, transactions, 1)
 print(multi)
+
+#exercise
+
+
+# Use map to print the square of each numbers rounded
+# to three decimal places
+my_floats = [4.35, 6.09, 3.25, 9.77, 2.16, 8.88, 4.59]
+
+# Use filter to print only the names that are less than 
+# or equal to seven letters
+my_names = ["olumide", "akinremi", "josiah", "temidayo", "omoseun"]
+
+# Use reduce to print the product of these numbers
+my_numbers = [4, 6, 9, 23, 5]
+
+# Fix all three respectively.
+map_result = list(map(lambda x: round(x, 3), my_floats))
+filter_result = list(filter(lambda name: len(name) <= 7, my_names))
+reduce_result = reduce(lambda num1, num2: num1 + num2, my_numbers, 0)
+
+print(map_result)
+print(filter_result)
+print(reduce_result)
