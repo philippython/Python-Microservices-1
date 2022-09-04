09dc25f0f275b2b8bf571da79b6b294660d2cc0b
odulaja philip <odulajaphilip@gmail.com>
2022-09-04 14:25:01 +0100

    filter method

--- comments/app/core/tests.py
+++ comments/app/core/tests.py
@@ -9,3 +9,19 @@ def multipy(*args):
     return sum
 
 print(multipy(6, 7, 8, 8))
+
+# mapping with one parameter
+transactions = [1200, 890, 456, 787, 5675]
+charge = lambda credit : credit - 500
+net_transactions = list(map(charge, transactions))
+
+print(net_transactions)
+
+#  mapping with two parameters
+circle_areas = [3.56773, 5.57668, 4.00914, 56.24241, 9.01344, 32.00013]
+result = list(map(round, circle_areas, range(1, 7)))
+print(result)
+
+# filtering 
+past = list(filter(lambda word :  'd' in word, ["laughed", "pray", "killed", "raped", "race"]))
+print(past)
